2c066996c9510dea9d5ed576a8018262e14c05f3
cecilia-la-place <claplace@asu.edu>
2017-02-20 01:04:24 -0700

    Issue 40: correct savings.java class structure

--- given/src/banking/primitive/core/Savings.java
+++ given/src/banking/primitive/core/Savings.java
@@ -12,31 +12,6 @@ package banking.primitive.core;
   Description: Class to organize the values pertaining to savings accounts
 */
 public class Savings extends Account {
-	private static final long serialVersionUID = 111L;
-
-	private int _numWithdraws = 0;
-	private float _depositFee = 0.50F;
-	private float _withdrawFee = 1.0F;
-	private int _withdrawLimit = 3;
-	/**
- 		Method: Savings
-		Inputs: String name
- 		Returns:
-		Description: Constructor for savings
-	*/
-	public Savings(String name) {
-		super(name);
-	}
-	/**
- 		Method: Savings
-		Inputs: String name, float balance
- 		Returns:
-
-		Description: Constructor for savings
-	*/
-	public Savings(String name, float balance) throws IllegalArgumentException {
-		super(name, balance);
-	}
 /**
 	  Method: deposit
 	  Inputs: float amount, float balance
@@ -58,12 +33,49 @@ public class Savings extends Account {
 	}
 
 	/**
+		Method: getType
+		Inputs:
+		Returns: String
+		Description: Returns the type of account this is
+	*/
+	public String getType() {
+		return "Savings";
+	}
+	/**
+		Method: toString
+		Inputs:
+		Returns: String
+		Description: Returns information about the account
+	*/
+	/**
+		Method: Savings
+	Inputs: String name
+		Returns:
+	Description: Constructor for savings
+	*/
+	public Savings(String name) {
+		super(name);
+	}
+	/**
+			Method: Savings
+		Inputs: String name, float balance
+			Returns:
+	
+		Description: Constructor for savings
+	*/
+	public Savings(String name, float balance) throws IllegalArgumentException {
+		super(name, balance);
+	}
+	public String toString() {
+		return "Savings: " + getName() + ": " + getBalance();
+	}
+	/**
 	  Method: withdraw
 	  Inputs: float amount
 	  Returns: boolean
 
-	  Description: Withdraws money from Savingsa ccount, boolean if it is successful or not
-		More than the withdrawl limit causes a withdrawl fee to incur. If the balance reaches
+	  Description: Withdraws money from Savings account, boolean if it is successful or not
+		More than the withdrawal limit causes a withdrawal fee to incur. If the balance reaches
 		below 0, the account enters an overdrawn state.
 	*/
 	public boolean withdraw(float amount) {
@@ -85,23 +97,11 @@ public class Savings extends Account {
 		}
 		return false;
 	}
+	
+	private static final long serialVersionUID = 111L;
 
-	/**
-		Method: getType
-		Inputs:
-		Returns: String
-		Description: Returns the type of account this is
-	*/
-	public String getType() {
-		return "Savings";
-	}
-	/**
-		Method: toString
-		Inputs:
-		Returns: String
-		Description: Returns information about the account
-	*/
-	public String toString() {
-		return "Savings: " + getName() + ": " + getBalance();
-	}
+	private int _numWithdraws = 0;
+	private float _depositFee = 0.50F;
+	private float _withdrawFee = 1.0F;
+	private int _withdrawLimit = 3;
 }
