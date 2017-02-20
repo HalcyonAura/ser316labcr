2df7f605b67a2e3197f5ac4a398625ecaae9d7f5
pahorton <pahorton@asu.edu>
2017-02-19 21:23:56 -0700

    added method banners

--- given/src/banking/primitive/core/Savings.java
+++ given/src/banking/primitive/core/Savings.java
@@ -13,23 +13,39 @@ public class Savings extends Account {
 	private float depositFee = 0.50F;
 	private float withdrawFee = 1.0F;
 	private int withdrawLimit = 3;
+	/**
+ 		Method: Savings
+		Inputs: String name
+ 		Returns:
 
+		Description: Constructor for savings
+	*/
 	public Savings(String name) {
 		super(name);
 	}
+	/**
+ 		Method: Savings
+		Inputs: String name, float balance
+ 		Returns:
 
+		Description: Constructor for savings
+	*/
 	public Savings(String name, float balance) throws IllegalArgumentException {
 		super(name, balance);
 	}
+/**
+	  Method: deposit
+	  Inputs: float amount, float balance
+	  Returns: boolean
 
-	/**
-	 * A deposit comes with a fee of 50 cents per deposit
-	 */
+	  Description: Deposits money into a non closed savings account, boolean if it is successful or not.
+		All deposits have a depositFee taken from them.
+	*/
 	public boolean deposit(float amount) {
-		if (getState() != State.CLOSED && amount > 0.0f) {
+		if (getState() != STATE.CLOSED && amount > 0.0f) {
 			balance = balance + amount - depositFee;
 			if (balance >= 0.0f) {
-				setState(State.OPEN);
+				setState(STATE.OPEN);
 			}
 			return true;
 		}
@@ -37,25 +53,43 @@ public class Savings extends Account {
 	}
 
 	/**
-	 * A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal.
-	 * An account whose balance dips below 0 is in an OVERDRAWN state
-	 */
+	  Method: withdraw
+	  Inputs: float amount
+	  Returns: boolean
+
+	  Description: Withdraws money from Savingsa ccount, boolean if it is successful or not
+		More than the withdrawl limit causes a withdrawl fee to incur. If the balance reaches
+		below 0, the account enters an overdrawn state.
+	*/
 	public boolean withdraw(float amount) {
-		if (getState() == State.OPEN && amount > 0.0f) {
+		if (getState() == STATE.OPEN && amount > 0.0f) {
 			balance = balance - amount;
 			numWithdraws++;
 			if (numWithdraws > withdrawLimit)
 				balance = balance - withdrawFee;
 			// KG BVA: should be < 0
 			if (balance < 0.0f) {
-				setState(State.OVERDRAWN);
+				setState(STATE.OVERDRAWN);
 			}
 			return true;
 		}
 		return false;
 	}
-	public String getType() { return "Savings"; }
+	/**
+		Method: getType
+		Inputs:
+		Returns: String
 
+		Description: Returns the type of account this is
+	*/
+	public String getType() { return "Savings"; }
+	/**
+		Method: toString
+		Inputs:
+		Returns: String
+
+		Description: Returns information about the account
+	*/
 	public String toString() {
 		return "Savings: " + getName() + ": " + getBalance();
 	}
