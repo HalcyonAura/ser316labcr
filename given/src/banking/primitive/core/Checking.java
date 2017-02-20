d496b894c3a6f1fb40178c4d1ef7bc754398e4d6
Paul Horton <pahorton@asu.edu>
2017-02-20 00:39:18 -0700

    Update Checking.java

--- given/src/banking/primitive/core/Checking.java
+++ given/src/banking/primitive/core/Checking.java
@@ -45,8 +45,8 @@ public class Checking extends Account {
 	*/
 	public boolean deposit(float amount) {
 		if (getState() != STATE.CLOSED && amount > 0.0f) {
-			balance = balance + amount;
-			if (balance >= 0.0f) {
+			_balance = _balance + amount;
+			if (_balance >= 0.0f) {
 				setState(STATE.OPEN);
 			}
 			return true;
@@ -87,13 +87,13 @@ public class Checking extends Account {
 	public boolean withdraw(float amount) {
 		if (amount > 0.0f) {
 			// KG: incorrect, last balance check should be >=
-			if (getState() == STATE.OPEN || (getState() == STATE.OVERDRAWN && balance >= -100.0f)) {
-				balance = balance - amount;
+			if (getState() == STATE.OPEN || (getState() == STATE.OVERDRAWN && _balance >= -100.0f)) {
+				_balance = _balance - amount;
 				_numWithdraws++;
 				if (_numWithdraws > 10){
-					balance = balance - 2.0f;
+					_balance = _balance - 2.0f;
 				}
-				if (balance < 0.0f) {
+				if (_balance < 0.0f) {
 					setState(STATE.OVERDRAWN);
 				}
 				return true;
