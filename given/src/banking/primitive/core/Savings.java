3adc16b4af0c177525fdf418de6cef0f9a822aad
pahorton <pahorton@asu.edu>
2017-02-19 21:06:36 -0700

    Added method banners

--- given/src/banking/primitive/core/Savings.java
+++ given/src/banking/primitive/core/Savings.java
@@ -6,25 +6,41 @@
   Description: This file holds the savings account class.
 */
 package banking.primitive.core;
-
 public class Savings extends Account {
 	private static final long serialVersionUID = 111L;
 	private int numWithdraws = 0;
 	private float depositFee = 0.50F;
 	private float withdrawFee = 1.0F;
 	private int withdrawLimit = 3;
+	/**
+	  Method: Savings
+	  Inputs: String name
+	  Returns:
 
+	  Description: Constructor for savings
+	*/
 	public Savings(String name) {
 		super(name);
 	}
+	/**
+	  Method: Savings
+	  Inputs: String name, float balance
+	  Returns:
 
+	  Description: Constructor for savings
+	*/
 	public Savings(String name, float balance) throws IllegalArgumentException {
 		super(name, balance);
 	}
 
 	/**
-	 * A deposit comes with a fee of 50 cents per deposit
-	 */
+	  Method: deposit
+	  Inputs: float amount, float balance
+	  Returns: boolean
+
+	  Description: Deposits money into a non closed savings account, boolean if it is successful or not.
+		All deposits have a depositFee taken from them.
+	*/
 	public boolean deposit(float amount) {
 		if (getState() != STATECLOSED && amount > 0.0f) {
 			balance = balance + amount - depositFee;
@@ -37,9 +53,14 @@ public class Savings extends Account {
 	}
 
 	/**
-	 * A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal.
-	 * An account whose balance dips below 0 is in an OVERDRAWN state
-	 */
+	  Method: withdraw
+	  Inputs: float amount
+	  Returns: boolean
+
+	  Description: Withdraws money from Savingsa ccount, boolean if it is successful or not
+		More than the withdrawl limit causes a withdrawl fee to incur. If the balance reaches
+		below 0, the account enters an overdrawn state.
+	*/
 	public boolean withdraw(float amount) {
 		if (getState() == STATEOPEN && amount > 0.0f) {
 			balance = balance - amount;
@@ -54,8 +75,21 @@ public class Savings extends Account {
 		}
 		return false;
 	}
-	public String getType() { return "Savings"; }
+	/**
+		Method: getType
+		Inputs:
+		Returns: String
 
+		Description: Returns the type of account this is
+	*/
+	public String getType() { return "Savings"; }
+	/**
+		Method: toString
+		Inputs:
+		Returns: String
+
+		Description: Returns information about the account
+	*/
 	public String toString() {
 		return "Savings: " + getName() + ": " + getBalance();
 	}
