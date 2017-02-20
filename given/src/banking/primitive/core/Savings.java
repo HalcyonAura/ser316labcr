93b5266a0e8ea9c061bff06b8c72923cac29c936
pahorton <pahorton@asu.edu>
2017-02-19 21:15:03 -0700

    Revert "Added method banners"
    This reverts commit 3adc16b4af0c177525fdf418de6cef0f9a822aad.

--- given/src/banking/primitive/core/Savings.java
+++ given/src/banking/primitive/core/Savings.java
@@ -6,41 +6,25 @@
   Description: This file holds the savings account class.
 */
 package banking.primitive.core;
+
 public class Savings extends Account {
 	private static final long serialVersionUID = 111L;
 	private int numWithdraws = 0;
 	private float depositFee = 0.50F;
 	private float withdrawFee = 1.0F;
 	private int withdrawLimit = 3;
-	/**
-	  Method: Savings
-	  Inputs: String name
-	  Returns:
 
-	  Description: Constructor for savings
-	*/
 	public Savings(String name) {
 		super(name);
 	}
-	/**
-	  Method: Savings
-	  Inputs: String name, float balance
-	  Returns:
 
-	  Description: Constructor for savings
-	*/
 	public Savings(String name, float balance) throws IllegalArgumentException {
 		super(name, balance);
 	}
 
 	/**
-	  Method: deposit
-	  Inputs: float amount, float balance
-	  Returns: boolean
-
-	  Description: Deposits money into a non closed savings account, boolean if it is successful or not.
-		All deposits have a depositFee taken from them.
-	*/
+	 * A deposit comes with a fee of 50 cents per deposit
+	 */
 	public boolean deposit(float amount) {
 		if (getState() != STATE.CLOSED && amount > 0.0f) {
 			balance = balance + amount - depositFee;
@@ -53,14 +37,9 @@ public class Savings extends Account {
 	}
 
 	/**
-	  Method: withdraw
-	  Inputs: float amount
-	  Returns: boolean
-
-	  Description: Withdraws money from Savingsa ccount, boolean if it is successful or not
-		More than the withdrawl limit causes a withdrawl fee to incur. If the balance reaches
-		below 0, the account enters an overdrawn state.
-	*/
+	 * A withdrawal. After 3 withdrawals a fee of $1 is added to each withdrawal.
+	 * An account whose balance dips below 0 is in an OVERDRAWN state
+	 */
 	public boolean withdraw(float amount) {
 		if (getState() == STATE.OPEN && amount > 0.0f) {
 			balance = balance - amount;
@@ -75,21 +54,8 @@ public class Savings extends Account {
 		}
 		return false;
 	}
-	/**
-		Method: getType
-		Inputs:
-		Returns: String
-
-		Description: Returns the type of account this is
-	*/
 	public String getType() { return "Savings"; }
-	/**
-		Method: toString
-		Inputs:
-		Returns: String
 
-		Description: Returns information about the account
-	*/
 	public String toString() {
 		return "Savings: " + getName() + ": " + getBalance();
 	}
